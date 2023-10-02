d34c94b0711530398fbeeb9b1c8ca560eef504c1
Chin-Yun Yu <cy006@bluck.eecs.qmul.ac.uk>
2023-10-02 15:20:39 +0100

    feat: reduce keyword

--- wav.py
+++ wav.py
@@ -5,6 +5,7 @@ import torch
 from torch.utils.data import Dataset
 from tqdm import tqdm
 import torchaudio
+from pathlib import Path
 
 
 class WAVDataset(Dataset):
@@ -12,14 +13,14 @@ class WAVDataset(Dataset):
     Wave-file-only Dataset.
     """
 
-    def __init__(self,
-                 data_dir: str,
-                 segment: int,
-                 overlap: int = 0):
+    def __init__(
+        self, data_dir: str, segment: int, overlap: int = 0, reduce: bool = False
+    ):
         assert segment > overlap and overlap >= 0
         self.segment = segment
         self.data_path = os.path.expanduser(data_dir)
         self.hop_size = segment - overlap
+        self.reduce = reduce
 
         self.waves = []
         self.sr = None
@@ -28,18 +29,15 @@ class WAVDataset(Dataset):
         file_chunks = []
 
         print("Gathering training files ...")
-        for f in tqdm(sorted(os.listdir(self.data_path))):
-            if f.endswith('.wav'):
-                filename = os.path.join(self.data_path, f)
-                meta = torchaudio.info(filename)
-                self.files.append(filename)
-                file_chunks.append(
-                    max(0, meta.num_frames - segment) // self.hop_size + 1)
+        for filename in tqdm(sorted(Path(self.data_path).glob("**/*.wav"))):
+            meta = torchaudio.info(filename)
+            self.files.append(filename)
+            file_chunks.append(max(0, meta.num_frames - segment) // self.hop_size + 1)
 
-                if not self.sr:
-                    self.sr = meta.sample_rate
-                else:
-                    assert meta.sample_rate == self.sr
+            if not self.sr:
+                self.sr = meta.sample_rate
+            else:
+                assert meta.sample_rate == self.sr
 
         self.size = sum(file_chunks)
         self.boundaries = np.cumsum(np.array([0] + file_chunks))
@@ -51,10 +49,14 @@ class WAVDataset(Dataset):
         bin_pos = np.digitize(index, self.boundaries[1:], right=False)
         f = self.files[bin_pos]
         offset = (index - self.boundaries[bin_pos]) * self.hop_size
-        x = torchaudio.load(f, frame_offset=offset,
-                            num_frames=self.segment)[0].mean(0)
-
+        x = torchaudio.load(f, frame_offset=offset, num_frames=self.segment)[0]
         # this should not happen but I just want to make sure
-        if x.numel() < self.segment:
-            x = torch.cat([x, x.new_zeros(self.segment - x.numel())])
+        if x.shape[1] < self.segment:
+            x = torch.cat(
+                [x, x.new_zeros([x.shape[0], self.segment - x.shape[1]])], dim=1
+            )
+
+        if self.reduce:
+            x = x.mean(0)
+
         return x
